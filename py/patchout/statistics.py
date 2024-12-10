2d7e9c6f8bc6bd0ed6e0b4f16e5aae2c1b68533b
SMM <m.m.shihov@gmail.com>
2024-12-10 20:46:34 +0300

    newstat

--- py/patchout/statistics.py
+++ py/patchout/statistics.py
@@ -11,6 +11,134 @@ TIME_STRIDE_PERCENT_OF_WINDOW_LEN=1
 
 COS_SIM = torch.nn.CosineSimilarity(dim=1)
 
+ORIGINAL_NAMES= [
+    "26_foo01_0_country_o.wav",
+    "28_foo03_0_jazz_o.wav",
+    "37_its_a_rainy_day_o.wav",
+    "aleksandr_pushnojj_pesenka_odnojj_gjorly_o.wav",
+    "aleksandr_pushnojj_valenki_o.wav",
+    "aura_dione_amp_rock_mafia_friends_o.wav",
+    "chajjf_argentina_jamajjka_5_0_o.wav",
+    "chajjf_dom_vverkh_dnom_o.wav",
+    "jurijj_vizbor_milaja_moja_o.wav",
+    "korol_i_shut_mariya_o.wav",
+    "lolita_last_day_o.wav",
+    "rap_german_o.wav",
+    "rap_noggano_be_good_o.wav",
+]
+
+SIMILAR_GROUPS = [
+    [
+        "26_foo01_0_country_d1.wav",
+        "26_foo01_0_country_d2.wav",
+        "26_foo01_0_country_o.wav",
+        "26_foo01_1_country_o.wav",
+    ], [
+        "28_foo03_0_jazz_d1.wav",
+        "28_foo03_0_jazz_d2.wav",
+        "28_foo03_0_jazz_o.wav",
+    ], [
+        "37_its_a_rainy_day_d1.wav",
+        "37_its_a_rainy_day_d2.wav",
+        "37_its_a_rainy_day_o.wav",
+    ], [
+        "jurijj_vizbor_milaja_moja_d1.wav",
+        "jurijj_vizbor_milaja_moja_d2.wav",
+        "jurijj_vizbor_milaja_moja_o.wav",
+    ], [
+        "rap_german_d1.wav",
+        "rap_german_d2.wav",
+        "rap_german_o.wav",
+    ], [
+        "rap_noggano_be_good_d1.wav",
+        "rap_noggano_be_good_d2.wav",
+        "rap_noggano_be_good_o.wav",
+    ], [
+        "korol_i_shut_mariya_d1.wav",
+        "korol_i_shut_mariya_o.wav",
+    ], [
+        "aleksandr_pushnojj_valenki_d1.wav",
+        "aleksandr_pushnojj_valenki_o.wav",
+    ], [
+        "lolita_last_day_d1.wav",
+        "lolita_last_day_o.wav",
+    ], [
+        "chajjf_argentina_jamajjka_5_0_d1.wav",
+        "chajjf_argentina_jamajjka_5_0_d2.wav",
+        "chajjf_argentina_jamajjka_5_0_o.wav",
+    ], [
+        "chajjf_dom_vverkh_dnom_d1.wav",
+        "chajjf_dom_vverkh_dnom_d2.wav",
+        "chajjf_dom_vverkh_dnom_o.wav",
+    ], [
+        "aura_dione_amp_rock_mafia_friends_d1.wav",
+        "aura_dione_amp_rock_mafia_friends_d2.wav",
+        "aura_dione_amp_rock_mafia_friends_o.wav",
+    ], 
+#    [
+#        "38_slave_on_line_d1.wav",
+#        "38_slave_on_line_d2.wav",
+#        "38_slave_on_line_d3.wav",
+#        "38_slave_on_line_o.wav",
+#    ], [
+#        "37_its_a_rainy_day_d1.wav",
+#        "37_its_a_rainy_day_d2.wav",
+#        "37_its_a_rainy_day_o.wav",
+#    ]  
+]
+
+NONSIMILAR_GROUPS = [
+    [
+        "blood_group_korea.wav",
+        "rape_me.wav",
+        "aleksandr_pushnojj_valenki_o.wav",
+    ],
+    [
+        "nirvana_rape_me_cover.wav",
+        "blood_group_in_german.wav",
+        "aleksandr_pushnojj_valenki_o.wav",
+    ],
+    [
+        "abba_gimme_gimme_gimme_a_man_after_midnight.wav",
+        "alizee_la_isla_bonita.wav",
+        "b_2_letchik.wav",
+        "basement_jaxx_get_me_off.wav",
+        "blood_group_karas.wav",
+        "blood_group_korea.wav",
+        "cellophane_fire.wav",
+        "chajjf_argentina_jamajjka_5_0_o.wav",
+        "chajjf_oranzhevoe_nastroenie.wav",
+        "circle_in_the_sand.wav",
+        "classical_piano_by_debussy_the_girl_with_the_flaxen_hair_120128.wav",
+        "corvet.wav",
+        "daft_punk_recognizer_52.wav",
+        "daft_punk_rinzler_70.wav",
+        "dark_synthwave_spectral_251688.wav",
+        "elena_terleeva_zaberi_solnce_s_soboyu.wav",
+        "energetic_rockabilly_instrumental_track_253093.wav",
+        "enigma_principles_of_lust.wav",
+        "german_was_wollen_wir_trinken_rock.wav",
+        "ice_t_gangsta_rap.wav",
+        "jesika_jay_cassablanka.wav",
+        "little_big_polyushko_polye.wav",
+        "madonna_youll_see.wav",
+        "mylene_farmer_appelle_mon_numero.wav",
+        "mylene_farmer_pourvu_quelles.wav",
+        "nirvana_lithium.wav",
+        "nol_lenin.wav",
+        "prodigy_breathe.wav",
+        "aleksandr_pushnojj_valenki_o.wav",
+        "rape_me.wav",
+        "rap_ni.wav",
+        "sam_brown_stop.wav",
+        "sektor_gaza_opyat_segodnya.wav",
+        "side_chick_158056.wav",
+        "skull_duggrey_ni_ni_ni.wav",
+        "splatter_party.wav",
+        "wagner_flying_holland.wav",
+        "zivert_love.wav",
+    ]
+]
 
 # functions
 def similarity(x, y):
@@ -27,7 +155,7 @@ def computeAudioSimilarity(x, y):
         i = i + 1
     
     print("AudioSim(", x["name"], ", ", y["name"], ") = ", simSum / embLen)
-    
+
     return simSum / embLen
 
 ## считаем симилярити склеенного вектора
